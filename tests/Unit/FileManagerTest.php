5d40708edec6409b19424bf4818edc1535ebefde
Adriangnr <adriancaslagnr@gmail.com>
2017-04-16 21:52:01 -0300

    valido que haya sido cargado el archivo de cuentas.

---
+++ tests/Unit/FileManagerTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Adrian
+ * Date: 4/16/2017
+ * Time: 8:58 PM
+ */
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class FileManagerTest extends TestCase
+{
+
+}
