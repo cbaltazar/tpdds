6d9cde9ad1c0f3f6bfe0cc365770ae29cbcc117f
amansilla <adrian.mansilla@avantrip.com>
2017-06-12 15:45:50 -0300

    corrijo merge en mi branch

--- tests/Unit/Model/Domain/DomainManagers/DomainManagerTest.php
+++ tests/Unit/Model/Domain/DomainManagers/DomainManagerTest.php
@@ -11,14 +11,11 @@ class DomainManagerTest extends TestCase
     protected $formulaElement = null;
     protected $entity = null;
 
-    protected function setUp(){
-        $this->entity = new Cuenta();
-
-        $this->formulaElement = $this->createMock('App\Model\Domain\FormulaElements\AccountElement');
-
-        $this->ormConnection = $this->createMock('App\Model\ORMConnections\EloquentConnection');
+    protected function setUp(){$this->entity = new Cuenta();
+        $this->formulaElement = $this->getMockBuilder('App\Model\Domain\FormulaElements\AccountElement');
+        $this->ormConnection = $this->getMockBuilder('App\Model\ORMConnections\EloquentConnection')
+            ->setMethods(['findFormulaElementEntity'])->getMock();
         $this->ormConnection->method('findFormulaElementEntity')->willReturn($this->entity);
-
         $this->domainManager = $this->getMockForAbstractClass(DomainManager::class);
         $this->domainManager->setOrmConnection($this->ormConnection);
     }
