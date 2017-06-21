e57e758c9eebb8e3269969996b785847488aa0af
Adriangnr <adriancaslagnr@gmail.com>
2017-06-21 00:45:39 -0300

    abm de metodologias.

--- resources/views/method_detail.blade.php
+++ resources/views/method_detail.blade.php
@@ -4,6 +4,7 @@
 <link href="{{asset('css/plugins/iCheck/custom.css')}}" rel="stylesheet">
 <link href="{{asset('js/plugins/sweetalert/dist/sweetalert.css')}}" rel="stylesheet">
 @endsection
+
 @section ('content')
 <div class="wrapper wrapper-content animated fadeInUp">
   <div class="row">
@@ -51,11 +52,73 @@
                                   </thead>
                                   <tbody>
                                     @if( $methodologyObject != null && count($methodologyObject->reglas) > 0 )
-                                       <h1>TIENE {{  count($methodologyObject->reglas ) }} REGLAS</h1>
+                                        <input type="hidden" id="methodologyObject" value="{{ count($methodologyObject->reglas)+1 }}" />
+                                           @foreach($methodologyObject->reglas as $key=>$regla)
+                                               <tr class='rule' id='{{ $key+1 }}'>
+                                                   <td>
+                                                       <b class='ruleId'>{{ $key+1 }}</b>
+                                                   </td>
+                                                   <td>
+                                                       <select class='form-control element'>
+                                                           @foreach($elements as $elem)
+                                                                <option value='{{ $elem->nombre }}'>{{ $elem->nombre }}</option>
+                                                           @endforeach
+                                                       </select>
+                                                   </td>
+                                                   <td class='form-inline'>
+                                                       <select class='form-control condition' style='width:100%'>
+                                                           <option value='min'>menor</option>
+                                                           <option value='max'>mayor</option>
+                                                           <option value='minq'>menor que</option>
+                                                           <option value='maxq'>mayor que</option>
+                                                           <option value='asc'>creciente</option>
+                                                           <option value='dec'>decreciente</option>
+                                                       </select>
+                                                       <input type='text' class='form-control value' name='valueToCompare' style='width:0%; display: none;'>
+                                                   </td>
+
+                                                   <td>
+                                                       <select class='form-control from' name='from'>
+                                                           <option value='2010'>2010</option>
+                                                           <option value='2011'>2011</option>
+                                                           <option value='2012'>2012</option>
+                                                           <option value='2013'>2013</option>
+                                                           <option value='2014'>2014</option>
+                                                           <option value='2015'>2015</option>
+                                                           <option value='2016'>2016</option>
+                                                           <option value='2017'>2017</option>
+                                                       </select>
+                                                   </td>
+
+                                                   <td>
+                                                       <select class='form-control to' name='to'>
+                                                           <option value='2010'>2010</option>
+                                                           <option value='2011'>2011</option>
+                                                           <option value='2012'>2012</option>
+                                                           <option value='2013'>2013</option>
+                                                           <option value='2014'>2014</option>
+                                                           <option value='2015'>2015</option>
+                                                           <option value='2016'>2016</option>
+                                                           <option value='2017'>2017</option>
+                                                       </select>
+                                                   </td>
+
+                                                   <td>
+                                                       <select class='form-control function'>
+                                                           <option value='uni'>Unitaria</option>
+                                                           <option value='sum'>Sumatoria</option>
+                                                           <option value='avg'>Promedio</option>
+                                                           <option value='med'>Media</option>
+                                                       </select>
+                                                   </td>
+                                                   <td>
+                                                       <button type='button' name='button' class='btn btn-sm btn-warning deleteRule'><i class='fa fa-trash'></i></button>
+                                                   </td>
+                                               </tr>
+                                           @endforeach
                                         @else
                                         <h1>NO TIENE REGLAS</h1>
                                         @endif
-
                                   </tbody>
                               </table>
                               <button type="button" name="button" class="btn btn-md btn-primary col-sm-offset-10" id="addRule"><i class="fa fa-plus"></i> Agregar Regla</button>
@@ -78,164 +141,167 @@
 @endsection
 
 @section ('scripts')
-  <script src="{{asset('js/plugins/iCheck/icheck.min.js')}}"></script>
-  <script src="{{asset('js/plugins/sweetalert/dist/sweetalert.min.js')}}"></script>
-  <script>
+    <script src="{{asset('js/plugins/iCheck/icheck.min.js')}}"></script>
+    <script src="{{asset('js/plugins/sweetalert/dist/sweetalert.min.js')}}"></script>
+    <script>
+        var ruleId=1;
+        $(document).ready(function(){
+            $('.i-checks').iCheck({
+                checkboxClass: 'icheckbox_square-green',
+                radioClass: 'iradio_square-green',
+            });
+            if( $("#methodologyObject").val() == undefined){
+                createRuleRow(ruleId);
+            }else{
+                ruleId = parseInt($("#methodologyObject").val());
+            }
 
-    var ruleId=1;
-      $(document).ready(function(){
-          $('.i-checks').iCheck({
-              checkboxClass: 'icheckbox_square-green',
-              radioClass: 'iradio_square-green',
-          });
-          createRuleRow(ruleId);
+            setTimeout(function () {
+                $(".alert").slideUp(1500);
+            }, 2000);
+        });
 
-          setTimeout(function () {
-              $(".alert").slideUp(1500);
-          }, 2000);
-     });
+        //--------campo value------------------
+        $('body').on('change','.condition', function(){
+            var rowId ='#'+ $(this).parent().parent().attr('id');
+            if(this.value == "minq" || this.value == "maxq"){
+                $(rowId+' .value').show().width("10%");
+                $(rowId+' .condition').width("36%");
+            }else {
+                $(rowId+' .value').hide()
+                $(rowId+' .condition').width("75%");
+            }
+        })
+        //-----------DATE-VALIDATOR--------------------------
+        $('body').on('change','.from,.to',function(){
+            var rowId = $(this).parent().parent().attr('id');
+            var lastPeriod = $(this).data('lastPeriod');
+            var newPeriod = $(this).val();
 
-//--------campo value------------------
-  $('body').on('change','.condition', function(){
-    var rowId ='#'+ $(this).parent().parent().attr('id');
-    if(this.value == "minq" || this.value == "maxq"){
-      $(rowId+' .value').show().width("10%");
-      $(rowId+' .condition').width("36%");
-    }else {
-      $(rowId+' .value').hide()
-      $(rowId+' .condition').width("75%");
-    }
-  })
-//-----------DATE-VALIDATOR--------------------------
-    $('body').on('change','.from,.to',function(){
-      var rowId = $(this).parent().parent().attr('id');
-      var lastPeriod = $(this).data('lastPeriod');
-      var newPeriod = $(this).val();
+            if($('#'+rowId+' .from').val() > $('#'+rowId+' .to').val()){
+                sweetAlert("Ups...", "La fecha final no puede ser menor que la inicial");
+                $(this).val(lastPeriod)
+            }else {
+                $(this).data('lastPeriod', newPeriod)
+            }
+        })
+        //-----------MODALIDAD VALIDATOR--------------------------
+        $('body').on('change','.from,.to,.condition', function(){
+            var rowId ='#'+ $(this).parent().parent().attr('id');
+            if($(rowId+' .from').val() != $(rowId+' .to').val() && $(rowId+' .condition').val()!='asc' && $(rowId+' .condition').val()!='dec'){
+                $(rowId+' .function').attr("disabled",false);
+            }else{
+                $(rowId+ ' .function').attr("disabled",true).val("uni");
+            }
+        });
+        //----------DELETE RULE------------------
+        $('body').on('click','.deleteRule',function(){
+            var i;
 
-      if($('#'+rowId+' .from').val() > $('#'+rowId+' .to').val()){
-        sweetAlert("Ups...", "La fecha final no puede ser menor que la inicial");
-        $(this).val(lastPeriod)
-      }else {
-        $(this).data('lastPeriod', newPeriod)
-      }
-    })
-//-----------MODALIDAD VALIDATOR--------------------------
-     $('body').on('change','.from,.to,.condition', function(){
-       var rowId ='#'+ $(this).parent().parent().attr('id');
-       if($(rowId+' .from').val() != $(rowId+' .to').val() && $(rowId+' .condition').val()!='asc' && $(rowId+' .condition').val()!='dec'){
-         $(rowId+' .function').attr("disabled",false);
-       }else{
-         $(rowId+ ' .function').attr("disabled",true).val("uni");
-       }
-     });
-//----------DELETE RULE------------------
-     $('body').on('click','.deleteRule',function(){
-       var i;
+            if ($('#rules .rule').length>1) {
+                rowId= $(this).parent().parent().attr('id');
+                $('#'+rowId).remove()
+                for (i = 0; i < $('#rules .rule').length; i++) {
+                    $($('#rules .rule')[i]).find('.ruleId').html(i+1)
+                    $($('#rules .rule')[i]).attr('id',i);
+                };
+                ruleId = i+1 //que proxima que se genere parta del id de la ultima
+            }else{
+                sweetAlert("Ups...", "Necesitas al menos una regla");
+            };
+        });
+        //----------CREATE RULE-----
+        $("#addRule").click(function(){
+            createRuleRow(ruleId)
+        });
 
-       if ($('#rules .rule').length>1) {
-         rowId= $(this).parent().parent().attr('id');
-         $('#'+rowId).remove()
-         for (i = 0; i < $('#rules .rule').length; i++) {
-           $($('#rules .rule')[i]).find('.ruleId').html(i+1)
-           $($('#rules .rule')[i]).attr('id',i);
-         };
-         ruleId = i+1 //que proxima que se genere parta del id de la ultima
-       }else{
-         sweetAlert("Ups...", "Necesitas al menos una regla");
-       };
-     });
-//----------CREATE RULE-----
-      $("#addRule").click(function(){
-        createRuleRow(ruleId)
-      });
+        function createRuleRow(){
+            var elements = JSON.parse('{!! json_encode($elements) !!}');
 
-      function createRuleRow(){
-          var elements = JSON.parse('{!! json_encode($elements) !!}');
+            var htmlText = "<tr class='rule' id='"+ruleId+"'>"+
+                "<td><b class='ruleId'>"+ruleId+" </b></td>"+
+                "<td><select class='form-control element'>";
 
-          var htmlText = "<tr class='rule' id='"+ruleId+"'>"+
-                         "<td><b class='ruleId'>"+ruleId+"</b></td>"+
-                         "<td><select class='form-control element'>";
+            elements.forEach(function ( item ) {
+                htmlText += "<option value='"+ item.nombre +"'>"+item.nombre+"</option>";
+            });
 
-          elements.forEach(function ( item ) {
-             htmlText += "<option value='"+ item.nombre +"'>"+item.nombre+"</option>";
-          });
+            htmlText += "</select></td>"+
+                "<td class='form-inline'>"+
+                "<select class='form-control condition' style='width:100%'>"+
+                "<option value='min'>menor</option>"+
+                "<option value='max'>mayor</option>" +
+                "<option value='minq'>menor que</option>"+
+                "<option value='maxq'>mayor que</option>"+
+                "<option value='asc'>creciente</option>"+
+                "<option value='dec'>decreciente</option>"+
+                "</select>";
 
-          htmlText += "<\td>"+
-                      "<td class='form-inline'>"+
-                      "<select class='form-control condition' style='width:100%'>"+
-                        "<option value='min'>menor</option>"+
-                        "<option value='max'>mayor</option>" +
-                        "<option value='minq'>menor que</option>"+
-                        "<option value='maxq'>mayor que</option>"+
-                        "<option value='asc'>creciente</option>"+
-                        "<option value='dec'>decreciente</option>"+
-                      "</select>";
+            htmlText += "<input type='text' class='form-control value' name='valueToCompare' style='width:0%; display: none;'></td>"+
+                "<td><select class='form-control from' name='from'>";
+            for(var j=0;j<7;j++){
+                htmlText += "<option value='201"+j+"'>201"+j+"</option>";
+            }
 
-          htmlText += "<input type='text' class='form-control value' style='width:0%'></td>"+
-                      "<td><select class='form-control from' name='from'>";
-          for(var j=0;j<7;j++){
-              htmlText += "<option value='201"+j+"'>201"+j+"</option>";
-          }
+            htmlText += "</select></td>"+
+                "<td><select class='form-control to' name='to'>";
 
-          htmlText += "</td>"+
-                      "<td><select class='form-control to' name='to'>";
+            for(var j=0;j<7;j++){
+                htmlText += "<option value='201"+j+"'>201"+j+"</option>";
+            }
 
-          for(var j=0;j<7;j++){
-              htmlText += "<option value='201"+j+"'>201"+j+"</option>";
-          }
+            htmlText += "</select></td>"+
+                "<td>" +
+                "<select class='form-control function'>"+
+                "<option value='uni'>Unitaria</option>"+
+                "<option value='sum'>Sumatoria</option>"+
+                "<option value='avg'>Promedio</option>"+
+                "<option value='med'>Media</option>"+
+                "</select>"+
+                "</td>";
 
-          htmlText += "</td>"+
-                      "<td>" +
-                          "<select class='form-control function'>"+
-                              "<option value='uni'>Unitaria</option>"+
-                              "<option value='sum'>Sumatoria</option>"+
-                              "<option value='avg'>Promedio</option>"+
-                              "<option value='med'>Media</option>"+
-                          "</select>"+
-                      "</td>";
+            htmlText += "<td>"+
+                "<button type='button' name='button' class='btn btn-sm btn-warning deleteRule'><i class='fa fa-trash'></i></button>"+
+                "</td>"+
+                "</tr>";
 
-          htmlText += "<td>"+
-                        "<button type='button' name='button' class='btn btn-sm btn-warning deleteRule'><i class='fa fa-trash'></i></button>"+
-                      "</td>"+
-                    "</tr>";
+            $("#rules").append( htmlText );
 
-        $("#rules").append( htmlText );
+            $('#'+ruleId+'>td>.function').attr("disabled",true);
+            $('#'+ruleId+'>td>.value').hide();
+            $('#'+ruleId+' .from').data('lastPeriod', $('#'+ruleId+' .from').val());
+            $('#'+ruleId+' .to').data('lastPeriod', $('#'+ruleId+' .to').val());
+            ruleId++;
+        }
 
-        $('#'+ruleId+'>td>.function').attr("disabled",true);
-        $('#'+ruleId+'>td>.value').hide();
-        $('#'+ruleId+' .from').data('lastPeriod', $('#'+ruleId+' .from').val());
-        $('#'+ruleId+' .to').data('lastPeriod', $('#'+ruleId+' .to').val());
-        ruleId++;
-    }
+        $('#methodology-form').submit(function(){
+            var methodology = prepareRequest();
+            $("#jsonData").val(JSON.stringify(methodology));
+            console.log( $("#jsonData").val() );
+        });
 
-    $('#methodology-form').submit(function(){
-        var methodology = prepareRequest();
-        $("#jsonData").val(JSON.stringify(methodology));
-        console.log( $("#jsonData").val() );
-    });
+        function prepareRequest(){
+            var methodology = {};
+            methodology.name = $("#nombre").val();
+            methodology.description = $("#descripcion").val();
+            methodology.status = ($("#estado").is(':checked')) ? 1 : 0;
+            methodology.rules = [];
 
-      function prepareRequest(){
-          var methodology = {};
-          methodology.name = $("#nombre").val();
-          methodology.description = $("#descripcion").val();
-          methodology.status = ($("#estado").is(':checked')) ? 1 : 0;
-          methodology.rules = [];
+            var rows = $("#rules").children('tbody').children('tr');
+            rows.each(function( key, item ){
+                var rule = {};
+                rule.element = $(item).find('.element').val();
+                rule.condition = $(item).find('.condition').val();
+                rule.period = {};
+                rule.period.from = $(item).find('.from').val();
+                rule.period.to = $(item).find('.to').val();
+                rule.mode = $(item).find('.function').val();
 
-          var rows = $("#rules").children('tbody').children('tr');
-          rows.each(function( key, item ){
-              var rule = {};
-              rule.element = $(item).find('.element').val();
-              rule.condition = $(item).find('.condition').val();
-              rule.period = {};
-              rule.period.from = $(item).find('.from').val();
-              rule.period.to = $(item).find('.to').val();
-              rule.mode = $(item).find('.function').val();
+                methodology.rules.push( rule );
+            });
 
-              methodology.rules.push( rule );
-          });
+            return methodology;
+        }
 
-          return methodology;
-      }
-
-  </script>
+    </script>
 @endsection
