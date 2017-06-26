ddda4e435b5559db1a0e88c32d6274d7c781035e
amansilla <adrian.mansilla@avantrip.com>
2017-06-26 17:59:29 -0300

    agrego codigo de calculo de media.

--- app/Model/Domain/Rules/Rule.php
+++ app/Model/Domain/Rules/Rule.php
@@ -59,8 +59,18 @@ abstract class Rule
         return $result/count($values);
     }
 
-    public function med($values){
-
+    public function med($arr){
+        sort($arr);
+        $count = count($arr);
+        $middleval = floor(($count-1)/2);
+        if($count % 2) {
+            $median = $arr[$middleval];
+        } else {
+            $low = $arr[$middleval];
+            $high = $arr[$middleval+1];
+            $median = (($low+$high)/2);
+        }
+        return $median;
     }
 
     public function getValuesOfPeriods($companyId, $rule)
