92cae7b437b71631a41d987d7464edfcffef9729
Adriangnr <adriancaslagnr@gmail.com>
2017-10-30 01:09:10 -0300

    scripts para agregar las claves foraneas a las tablas. No anda la de indicadores, no se porque.

---
+++ database/migrations/2017_10_30_032335_add_foreing_key_cuentas_cuenta_empresa.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddForeingKeyCuentasCuentaEmpresa extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('cuenta_empresa', function (Blueprint $table) {
+            $table->foreign('cuenta_id')->references('id')->on('cuentas');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
