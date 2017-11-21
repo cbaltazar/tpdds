e913567d4ca59390ccb0a4e65050fa9595a5b641
amansilla <adrian.mansilla@avantrip.com>
2017-11-21 12:10:09 -0300

    prueba

--- app/Model/Entities/Empresa.php
+++ app/Model/Entities/Empresa.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Empresa extends Model
 {
+    public $timestamps = false;
+
     public function cuentas(){
         return $this->belongsToMany(Cuenta::class)->withPivot('periodo', 'monto');
     }
